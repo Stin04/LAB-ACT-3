93ccd4550afa92ccb37895a4632f0a3acdcf2866
Stin04 <justineidos04@gmail.com>
2025-03-09 12:06:37 +0800

    Add files via upload

---
+++ act3.py
@@ -0,0 +1,15 @@
+#ACTIVITY 3
+#JUSTINE IDOS -CPE 1
+def check_number():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print(f"{num} is an Even number.")
+    else:
+        print(f"{num} is an Odd number.")
+    user_input = input("Do you want to check another number? (yes/no): ")
+    if user_input == 'yes' or user_input == 'y':
+        check_number()
+    else:
+        print("Number checking done.")
+    
+check_number
